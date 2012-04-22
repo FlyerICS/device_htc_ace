bbd5835af2ef8cd78074cc8fdaaf482ec6479365
Drew Garen <dgflyerics@gmail.com>
2012-04-21 23:08:57 -0400

    HTC ACE Device Files
    Updated to work with Flyer

---
+++ ace.mk
@@ -0,0 +1,195 @@
+#
+# Copyright (C) 2011 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+## (1) First, the most specific values, i.e. the aspects that are specific to GSM
+
+PRODUCT_COPY_FILES += \
+    device/htc/ace/kernel/init.spade.rc:root/init.spade.rc \
+    device/htc/ace/kernel/ueventd.spade.rc:root/ueventd.spade.rc
+
+## (2) Also get non-open-source GSM-specific aspects if available
+$(call inherit-product-if-exists, vendor/htc/ace/ace-vendor.mk)
+$(call inherit-product-if-exists, vendor/twisted/twisted-vendor.mk)
+
+## (3)  Finally, the least specific parts, i.e. the non-GSM-specific aspects
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.com.google.clientidbase=android-htc \
+    ro.com.google.locationfeatures=1 \
+    ro.com.google.networklocation=1 \
+    ro.com.google.gmsversion=2.3_r3 \
+    ro.setupwizard.enable_bypass=1 \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y
+
+# Override /proc/sys/vm/dirty_ratio on UMS
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vold.umsdirtyratio=20
+
+DEVICE_PACKAGE_OVERLAYS += device/htc/ace/overlay
+
+# Devie XML Permissions
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
+
+# Device XML Properties
+PRODUCT_COPY_FILES += \
+    device/htc/ace/system/etc/voicemail-conf.xml:system/etc/voicemail-conf.xml \
+    device/htc/ace/system/etc/apns-conf.xml:system/etc/apns-conf.xml \
+    device/htc/ace/system/etc/gps.conf:system/etc/gps.conf
+
+PRODUCT_PACKAGES += \
+    lights.spade \
+    sensors.spade \
+    gps.spade
+
+# Keylayouts and Touchscreen
+PRODUCT_COPY_FILES += \
+    device/htc/ace/system/usr/idc/synaptics-rmi-touchscreen.idc:/system/usr/idc/synaptics-rmi-touchscreen.idc \
+    device/htc/ace/system/usr/idc/synaptics-rmi-touchscreen.idc:/system/usr/idc/atmel-touchscreen.idc \
+    device/htc/ace/system/usr/keychars/qwerty2.kcm.bin:system/usr/keychars/qwerty2.kcm.bin \
+    device/htc/ace/system/usr/keychars/qwerty.kcm.bin:system/usr/keychars/qwerty.kcm.bin \
+    device/htc/ace/system/usr/keychars/spade-keypad.kcm.bin:system/usr/keychars/spade-keypad.kcm.bin \
+    device/htc/ace/system/usr/keylayout/AVRCP.kl:system/usr/keylayout/AVRCP.kl \
+    device/htc/ace/system/usr/keylayout/h2w_headset.kl:system/usr/keylayout/h2w_headset.kl \
+    device/htc/ace/system/usr/keylayout/qwerty.kl:system/usr/keylayout/qwerty.kl \
+    device/htc/ace/system/usr/keylayout/spade-keypad.kl:system/usr/keylayout/spade-keypad.kl
+
+# Firmware
+PRODUCT_COPY_FILES += \
+    device/htc/ace/system/etc/firmware/bcm4329.hcd:system/etc/firmware/bcm4329.hcd \
+    device/htc/ace/system/etc/firmware/fw_bcm4329.bin:system/etc/firmware/fw_bcm4329.bin \
+    device/htc/ace/system/etc/firmware/fw_bcm4329_apsta.bin:system/etc/firmware/fw_bcm4329_apsta.bin \
+    device/htc/ace/system/etc/firmware/default.acdb:system/etc/firmware/default.acdb \
+    device/htc/ace/system/etc/firmware/default_org.acdb:system/etc/firmware/default_org.acdb
+
+# Audio DSP Profiles
+PRODUCT_COPY_FILES += \
+    device/htc/ace/system/etc/AIC3254_REG.csv:system/etc/AIC3254_REG.csv \
+    device/htc/ace/system/etc/AIC3254_REG_XD.csv:system/etc/AIC3254_REG_XD.csv \
+    device/htc/ace/system/etc/AdieHWCodec.csv:system/etc/AdieHWCodec.csv \
+    device/htc/ace/system/etc/CodecDSPID.txt:system/etc/CodecDSPID.txt \
+    device/htc/ace/system/etc/HP_Audio.csv:system/etc/HP_Audio.csv \
+    device/htc/ace/system/etc/HP_Video.csv:system/etc/HP_Video.csv \
+    device/htc/ace/system/etc/SPK_Combination.csv:system/etc/SPK_Combination.csv \
+    device/htc/ace/system/etc/soundimage/Sound_Bass_Booster.txt:system/etc/soundimage/Sound_Bass_Booster.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Blues.txt:system/etc/soundimage/Sound_Blues.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Classical.txt:system/etc/soundimage/Sound_Classical.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Country.txt:system/etc/soundimage/Sound_Country.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Dolby_A_HP.txt:system/etc/soundimage/Sound_Dolby_A_HP.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Dolby_A_SPK.txt:system/etc/soundimage/Sound_Dolby_A_SPK.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Dolby_V_HP.txt:system/etc/soundimage/Sound_Dolby_V_HP.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Dolby_V_SPK.txt:system/etc/soundimage/Sound_Dolby_V_SPK.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Dualmic.txt:system/etc/soundimage/Sound_Dualmic.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Dualmic_EP.txt:system/etc/soundimage/Sound_Dualmic_EP.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Dualmic_SPK.txt:system/etc/soundimage/Sound_Dualmic_SPK.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Jazz.txt:system/etc/soundimage/Sound_Jazz.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Latin.txt:system/etc/soundimage/Sound_Latin.txt \
+    device/htc/ace/system/etc/soundimage/Sound_New_Age.txt:system/etc/soundimage/Sound_New_Age.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Original.txt:system/etc/soundimage/Sound_Original.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Original_SPK.txt:system/etc/soundimage/Sound_Original_SPK.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Piano.txt:system/etc/soundimage/Sound_Piano.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Pop.txt:system/etc/soundimage/Sound_Pop.txt \
+    device/htc/ace/system/etc/soundimage/Sound_R_B.txt:system/etc/soundimage/Sound_R_B.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Rock.txt:system/etc/soundimage/Sound_Rock.txt \
+    device/htc/ace/system/etc/soundimage/Sound_SRS_A_HP.txt:system/etc/soundimage/Sound_SRS_A_HP.txt \
+    device/htc/ace/system/etc/soundimage/Sound_SRS_A_SPK.txt:system/etc/soundimage/Sound_SRS_A_SPK.txt \
+    device/htc/ace/system/etc/soundimage/Sound_SRS_V_HP.txt:system/etc/soundimage/Sound_SRS_V_HP.txt \
+    device/htc/ace/system/etc/soundimage/Sound_SRS_V_SPK.txt:system/etc/soundimage/Sound_SRS_V_SPK.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Treble_Booster.txt:system/etc/soundimage/Sound_Treble_Booster.txt \
+    device/htc/ace/system/etc/soundimage/Sound_Vocal_Booster.txt:system/etc/soundimage/Sound_Vocal_Booster.txt
+#    device/htc/ace/system/etc/audio_effects.conf:/system/etc/audio_effects.conf
+
+# Additional NAM Audio DSP Profiles to NAM Package
+PRODUCT_COPY_FILES += \
+    device/htc/ace/system/bin/modelid_cfg.sh:system/bin/modelid_cfg.sh \
+    device/htc/ace/system/etc/nam/default.acdb:system/etc/nam/default.acdb \
+    device/htc/ace/system/etc/nam/default_org.acdb:system/etc/nam/default_org.acdb \
+    device/htc/ace/system/etc/nam/AdieHWCodec.csv:system/etc/nam/AdieHWCodec.csv \
+    device/htc/ace/system/etc/nam/AIC3254_REG_DualMic_MCLK.csv:system/etc/nam/AIC3254_REG_DualMic_MCLK.csv \
+    device/htc/ace/system/etc/nam/CodecDSPID_MCLK.txt:system/etc/nam/CodecDSPID_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Treble_Booster_MCLK.txt:system/etc/nam/Sound_Treble_Booster_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Vocal_Booster_MCLK.txt:system/etc/nam/Sound_Vocal_Booster_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_SRS_A_SPK_MCLK.txt:system/etc/nam/Sound_SRS_A_SPK_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_SRS_V_HP_MCLK.txt:system/etc/nam/Sound_SRS_V_HP_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_SRS_V_SPK_MCLK.txt:system/etc/nam/Sound_SRS_V_SPK_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Jazz_MCLK.txt:system/etc/nam/Sound_Jazz_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Latin_MCLK.txt:system/etc/nam/Sound_Latin_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_New_Age_MCLK.txt:system/etc/nam/Sound_New_Age_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Original_MCLK.txt:system/etc/nam/Sound_Original_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Piano_MCLK.txt:system/etc/nam/Sound_Piano_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Pop_MCLK.txt:system/etc/nam/Sound_Pop_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_R_B_MCLK.txt:system/etc/nam/Sound_R_B_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Rock_MCLK.txt:system/etc/nam/Sound_Rock_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_SRS_A_HP_MCLK.txt:system/etc/nam/Sound_SRS_A_HP_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dualmic_SPK_MCLK.txt:system/etc/nam/Sound_Dualmic_SPK_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dualmic_EP_MCLK.txt:system/etc/nam/Sound_Dualmic_EP_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dualmic_MCLK.txt:system/etc/nam/Sound_Dualmic_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dolby_A_SPK_MCLK.txt:system/etc/nam/Sound_Dolby_A_SPK_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dolby_HP_MCLK.txt:system/etc/nam/Sound_Dolby_HP_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dolby_Spk_MCLK.txt:system/etc/nam/Sound_Dolby_Spk_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dolby_V_HP_MCLK.txt:system/etc/nam/Sound_Dolby_V_HP_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dolby_V_SPK_MCLK.txt:system/etc/nam/Sound_Dolby_V_SPK_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Dolby_A_HP_MCLK.txt:system/etc/nam/Sound_Dolby_A_HP_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Bass_Booster_MCLK.txt:system/etc/nam/Sound_Bass_Booster_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Blues_MCLK.txt:system/etc/nam/Sound_Blues_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Classical_MCLK.txt:system/etc/nam/Sound_Classical_MCLK.txt \
+    device/htc/ace/system/etc/nam/Sound_Country_MCLK.txt:system/etc/nam/Sound_Country_MCLK.txt
+
+# we have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+# High-density art, but English locale
+PRODUCT_LOCALES += en
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+PRODUCT_COPY_FILES += \
+    device/htc/ace/system/etc/vold.fstab:system/etc/vold.fstab
+
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := device/htc/ace/kernel/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+# Kernel Modules
+PRODUCT_COPY_FILES += \
+    device/htc/ace/kernel/lib/modules/qc_pcm_in.ko:system/lib/modules/qc_pcm_in.ko \
+    device/htc/ace/kernel/lib/modules/bcm4329.ko:system/lib/modules/bcm4329.ko
+
+# Script to signal boot completion for init.d
+PRODUCT_COPY_FILES += \
+    device/htc/ace/kernel/etc/init.d/100complete:system/etc/init.d/100complete
+
+# common msm7x30 configs
+$(call inherit-product, device/htc/msm7x30-common/msm7x30.mk)
+
+# media profiles and capabilities spec
+$(call inherit-product, device/htc/ace/media_htcaudio.mk)
+
+# stuff common to all HTC phones
+$(call inherit-product, device/htc/common/common.mk)
+
+$(call inherit-product, build/target/product/full_base.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := htc_ace
+PRODUCT_DEVICE := ace
+PRODUCT_MODEL := HTC Desire HD
+PRODUCT_MANUFACTURER := HTC
